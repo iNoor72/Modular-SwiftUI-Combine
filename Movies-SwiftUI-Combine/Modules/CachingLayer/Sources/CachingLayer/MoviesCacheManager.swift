d6f7a7f28135134de5b8ade0f4df704893c8a3c1
Noor El-Din Walid <noorwalid72@yahoo.com>
2024-11-25 15:32:47 +0200

    Added base for caching layer

---
+++ Movies-SwiftUI-Combine/Modules/CachingLayer/Sources/CachingLayer/MoviesCacheManager.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  CachingLayer
+//
+//  Created by Noor El-Din Walid on 25/11/2024.
+//
+
+import Foundation
+
+public final class MoviesCacheManager: MovieCacheManagerProtocol {
+    
+}
---
+++ Movies-SwiftUI-Combine/Modules/CachingLayer/Sources/CachingLayer/MoviesCacheManagerProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  MovieCacheManagerProtocol.swift
+//  CachingLayer
+//
+//  Created by Noor El-Din Walid on 25/11/2024.
+//
+
+
+public protocol MovieCacheManagerProtocol {
+    
+}
